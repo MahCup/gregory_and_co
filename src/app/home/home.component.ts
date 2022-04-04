0f5be1bc20bdfb1edeef9e6ddd539405f91c9dfd
MahCup <marcelacuperteixeira@gmail.com>
2022-04-04 11:58:05 -0300

    home vitrine - falta linkar página com busca

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = new Array();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getProducts(2);
+  }
+
+  async getProducts(page: any) {
+    const resposta = await fetch('http://localhost:8080/product/page', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ page: page })
+    });
+
+    const json = await resposta.json();
+
+    json.forEach((repo: any) => {
+
+      this.products.push({
+        "id": repo.id,
+        "nome": repo.nome,
+        "preco": repo.preco,
+        "numeracao": repo.numeracao,
+        "cor": repo.cor,
+        "palmilha_anti_odor": repo.palmilha_anti_odor ? "Possui" : "Não possui",
+        "material": repo.material,
+        "publico": repo.publico,
+        "tipo_de_fechamento": repo.tipo_de_fechamento,
+        "amortecedor": repo.amortecedor ? "Possui" : "Não possui",
+        "brand_id": repo.brand_id,
+        "image": "assets/images/" + repo.nome + ".jpg"
+      });
+
+    });
   }
 
 }
